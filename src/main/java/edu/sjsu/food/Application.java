8e1aa4a0e21593cae2af3dda23ff2fb1aa073efa
chris.hou <chrishou118@gmail.com>
2015-11-18 21:52:32 -0800

    first commit

---
+++ src/main/java/edu/sjsu/food/Application.java
@@ -0,0 +1,15 @@
+package edu.sjsu.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by yutao on 11/18/15.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
